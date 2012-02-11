608cc0018b4c21881288a1d52b96c667ed2329ca
Dan O'Huiginn <daniel@ohuiginn.net>
2012-02-11 08:44:38 -0500

    Fix png serving
    [last commit was just plain broken]

--- node/oed.js
+++ node/oed.js
@@ -1,9 +1,8 @@
 var app = require('express').createServer();
 var exec = require('child_process').exec;
 var spawn = require('child_process').spawn;
-var fs = require('fs');
 
-var jpeg_spawn = function(req, res){
+var png_for_page = function(req, res){
 
 	var page = req.param('page', '250');
 	var vol = req.param('vol', '6');
@@ -18,7 +17,6 @@ var jpeg_spawn = function(req, res){
 									convert.stdin.write(data);
 									});
 	convert.stdout.on('data', function(data){
-										console.log('writing binary');
 										res.write(data, 'binary');
 										});
 	convert.on('exit', function(code){
@@ -29,11 +27,19 @@ var jpeg_spawn = function(req, res){
 						 convert.stdin.end();
 						 });
 };
+var djvu_for_page = function(req, res){
+	//not yet implemented -- use /page/png or ohuiginn.net/oed
+	var page = req.param('page', '250');
+	var vol = req.param('vol', '6');
+	var format = req.param('format', 'png');
+	var djvuoptions =['-page=' + page, '-format=ppm', '/var/www/oed/oedvol' + vol + '.djvu'];
+  var ddjvu = spawn('ddjvu',  djvuoptions);
+	}
 
 app.get('/', function(req, res){
-				res.send('OK');
+				res.send('Nothing here. Try /page/png?vol=2&page=240');
 				});
-
 app.get('/page/png', png_for_page);
+app.get('/page/djvu', djvu_for_page);
 app.listen(8082);
 
